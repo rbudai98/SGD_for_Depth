4b21cd9e6b510beb0643d523ae67af77ed56f87b
rbudai98 <robert.budai@analog.com>
2022-02-15 18:56:35 +0200

    Added epoc learning fir Images
    Signed-off-by: rbudai98 <robert.budai@analog.com>

--- src/sgd.cpp
+++ src/sgd.cpp
@@ -1,220 +1,7 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <bits/stdc++.h>
-
-#define sample_number 1000
-
+#include "../include/sgd.h"
 using namespace std;
 
-float sigmoid(double x)
-{
-    //cout<<x<<": ";
-    //cout<<1.0/(1+exp(-x))<<"; "<<endl;
-    return((float)1.0/((float)1.0+(float)exp(-x)));
-}
-
-void cout_char(char a)
-{
-    for (int i=7;i>=0;i--)
-        cout<<(1  & (a >> i))<<"";
-    cout<<endl;
-}
-
-class Image
-{
-private:
-    float *tomb;
-    int image_value;
-public:
-    Image()
-    {
-        tomb = (float*)malloc(sizeof(float)*784);
-    }
-    ~Image()
-    {
-        free(tomb);
-    }
-    void load(char * buffer, int start_p, int end_p)
-    {
-        for (int i=0;i<end_p-start_p;i++)
-        {
-            tomb[i] = (float)buffer[i+start_p];
-        }
-    }
-    float * get_values()
-    {
-        return tomb;
-    }
-    void set_image_value(int temp)
-    {
-        image_value = temp;
-    }
-    int get_image_value()
-    {
-        return image_value;
-    }
-    void print()
-    {
-        for (int i=0;i<784;i++)
-        {
-            if (tomb[i] == 0)
-                cout<<" ";
-            else
-                cout<<"#";
-            if (i%28==0 && i!=0)
-                cout<<endl;
-        }
-    }
-};
-
-class Layer
-{
-private:
-    float **net;
-    float *net_value;
-    float *weight_bias;
-    int weights,nodes;
-
-public:
-
-    static int aux;
-    int current;
-
-    Layer(int nodes_tmp, int weights_tmp)
-    {
-        weights = weights_tmp;
-        nodes = nodes_tmp;
-        net = (float **)malloc(sizeof(float*)*nodes_tmp);
-        for (int i=0;i<nodes_tmp;i++)
-        {
-            net[i] = (float*)malloc(sizeof(float)*weights_tmp);
-        }
-        net_value = (float *) malloc(sizeof(float)*nodes_tmp);
-        weight_bias = (float *) malloc(sizeof(float)*weights_tmp);
-        current = aux++;
-        cout<<"Layer "<<current<<" has been created with "<<nodes<<" nodes and "<<weights<<" weights, each."<<endl;
-
-    }
-    ~Layer()
-    {
-        for (int i=0;i<weights;i++)
-        {
-            free(net[i]);
-        }
-        free(net);
-        free(net_value);
-        free(weight_bias);
-    }
-    void random_fillup()
-    {
-        for (int i=0;i<nodes;i++)
-        {
-            for (int j=0;j<weights;j++)
-            {
-                net[i][j] = (float)(rand()%200 -100)/100.0;
-                if (i==0)
-                    weight_bias[j] = (float)(rand()%200 -100)/100.0;
-            }
-            net_value[i] = 0.0;
-        }
-        cout<<"Layer "<<current<<" weight values had been updated randomly."<<endl;
-
-    }
-    void print()
-    {
-        for (int i=0;i<nodes;i++)
-        {
-            for (int j=0;j<weights;j++)
-            {
-                cout<<net[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
-    void load_node_value(float *tmp)
-    {
-        net_value = tmp;
-        cout<<"Layer "<<current<<" node values have been updated."<<endl;
-    }
-    float * get_next_values()
-    {
-        float * tmp = (float*) malloc(sizeof(float)*weights);
-        double dtmp;
-        for (int i=0;i<weights;i++)
-        {
-            dtmp=0.0;
-            for (int j=0;j<nodes;j++)
-            {
-                dtmp += net_value[j]*net[j][i];
-            }
-            dtmp+=weight_bias[i];
-
-            tmp[i] = (float)sigmoid(dtmp);
-        }
-        return tmp;
-    }
-    float * get_node_values()
-    {
-        return net_value;
-    }
-    float get_node_weight(int n, int w)
-    {
-        return net[n][w];
-    }
-    void set_node_weight(int n, int w, float temp)
-    {
-        net[n][w] = temp;
-    }
-    float get_weight_bias(int n)
-    {
-        return weight_bias[n];
-    }
-    void set_weight_bias(int n, float b)
-    {
-        weight_bias[n] = b;
-    }
-};
-
-int Layer::aux = 1;
-
-void print_buffer(char *buffer, char * buffer_labels, int lSize)
-{
-    int i=0;
-    char ch1{176};
-    for (i=16;i<lSize;i++)
-    {
-        if (buffer[i]==0)
-        cout<<" ";
-        else
-        cout<<ch1;
-
-        if ((i-16)%28==0)
-        cout<<endl;
-        if ((i-16)%784 == 0)
-        {
-            cout<<"#########################################"<<endl;
-            cout<<buffer_labels[(i-16)/784+8]+0<<endl;
-            cout<<"#########################################"<<endl;
-        }
-    }
-}
-void print_images(int **images)
-{
-    for (int i=0;images[i] != NULL;i++)
-    {
-        for (int j=0;images[i][j]!=NULL;j++)
-        {
-            cout<<images[i][j];
-        }
-        cout<<endl;
-    }
-}
-int main ()
+int main()
 {
     FILE * pFile;
     FILE * pFile_labels;
@@ -251,47 +38,124 @@ int main ()
     cout<<"Image values loaded into \'buffer\' and labels into \'buffer_labels\'."<<endl;
     /* the whole file is now loaded in the memory buffer. */
 
+    cout<<"Starting program\n";
     //print_buffer(buffer, buffer_labels, lSize);
 
-    //loading values
+    //loading image into the Image object
     Image X[sample_number];
     for (int i=0;i<sample_number;i++)
     {
-        X[i].load(buffer,16+(i*784),16+((i+1)*784));
+        X[i].load(buffer,16+(i*image_size),16+((i+1)*image_size));
         X[i].set_image_value(buffer_labels[i+8]);
     }
     free(buffer);
-    cout<<"Images segmented."<<endl;
+	free(buffer_labels);
+    cout<<"Images segmented.\n############\n"<<endl;
+
+
+    //Creating the network
+    cout<<"Creating network (...)\n";
+
+
+	/*
+	//**************************************************************************************************************************
+	//ACTUAL PROGRAM
+    
+	int network_data[3];
+    network_data[0] = image_size;
+    network_data[1] = 30;
+    network_data[2] = 10;
+    //Layer * network = create_network(2,network_data);
+    Network * net  = new Network(2,network_data);
+	delta delta_net(2, network_data);
+    cout<<"Network created\n#############\n\n";
+
+
+	for (int i=0;i<epoch_number;i++)
+	{
+		int* perm = generate_permutation_of_number(sample_number);
+		cout << i <<". ";
+		learn_epoch(delta_net, net, X, perm);
+	}
+
+
+    cout<<endl<<"Progam ended\n"<<endl;
+    
+
+
+	
+	// terminate
+	//test phase
+
+	int current;
+	cout << "#################################################################\n\n";
+	int* perm = generate_permutation_of_number(sample_number);
+	for (int i = 0; i < 20; i++)
+	{
+		//current = (int)(rand() %1000 + rand()%100);
+		//X[current].print();
+		current = i;
+		cout << current << endl;
+		X[perm[current]].print();
+		cout << endl;
+		cout << "\nActual value: " << X[perm[current]].get_image_value() << endl << "Learned value: \n";
+		net->print_Get_network_output(X[perm[current]].get_values());
+		cout << "#################################################################\n\n";
+	}
+	//**************************************************************************************************************************
+	
+	*/
+
+
+	
+	//**************************************************************************************************************************
+	//#	 TEST	###
+	int network2_data[2] = {10,10};
+	Network* net2 = new Network(1, network2_data);
+	cout << "Network\n#################################################\n";
+	//net2->print_all_weights_and_biases();
+	cout << "##########################################################\n";
+	delta delta_net2(1, network2_data);
+	//cout << "####################################\nNetwork 2 created\n";
+
+	char test_input[10] = { 0,0,1,0,0,0,0,0,0,0 };
+	Image X_test;
+	X_test.load(test_input, 0, 10);
+	X_test.set_image_value(2);
+
+	//net2->print_Get_network_output(X_test.get_values());
+	for (int i = 0; i < 100; i++)
+	{
+		delta_net2.upload_with_zeros();
+		net2->calculate_errors(X_test);
+		delta_net2.add_new_delta_values(net2, X_test);
+		delta_net2.update_Layer_values_with_derivatives(net2);
+		//delta_net2.print_all_values();
+	}
+
+	//net2->print_Get_network_output(X_test.get_values());
+
+	cout << "Network\n#################################################\n";
+	//net2->print_all_weights_and_biases();
+	cout << "##########################################################\n";
+	cout << endl << endl << "Network output:\n";
+	net2->print_Get_network_output(X_test.get_values());
+	//*******************************************************************************************************************************
+	
+	
 
 
 
-    Layer layer_1(784,30);
-    layer_1.random_fillup();
-    Layer layer_2(30,10);
-    layer_2.random_fillup();
+	//cout<<"Getting the output value of the network:\n\n";
+	//net->print_Get_network_output(X[1].get_values());
+	//net.print_errors();
+	//for back propagation	 we need to calculate the last layers neuron errors, from there backward we calculate the previous layers eroors. If we have all this we can calculate the
+	//cout << "Calculating the error of the first input image:\n";
+	//net->calculate_errors(X[1]);
+	//net->print_errors();
+	//creating the delta structure and uploading all the values woth 0
+	//delta_net.print_all_values();
 
-    layer_1.load_node_value(X[1].get_values());
-
-    float * value_temp = layer_1.get_next_values();
-    cout<<"Layer 2: ";
-    for (int i=0;i<30;i++)
-    {
-        cout<<value_temp[i]<<"  ";
-    }
-    cout<<endl;
-    layer_2.load_node_value(value_temp);
-
-    value_temp = layer_2.get_next_values();
-    cout<<"Layer 3: ";
-    for (int i=0;i<10;i++)
-    {
-        cout<<value_temp[i]<<"  ";
-    }
-
-
-
-    // terminate
     fclose (pFile);
-    free (buffer);
     return 0;
 }
