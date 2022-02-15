9ef70739cb1abd6b7c02fe133d249f3535ef2855
rbudai98 <robert.budai@analog.com>
2022-02-15 17:06:38 +0200

    SGB -initial version
    Signed-off-by: rbudai98 <robert.budai@analog.com>

--- src/sgd.cpp
+++ src/sgd.cpp
@@ -1,73 +1,297 @@
-#include <iostrea.h>
-#include "../include/sgd.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <bits/stdc++.h>
 
-Frame::Frame(int width, int height)
-{
-    width = this.width;
-    height = this.height;
-    values = (float *)malloc(width * height * sizeof(float));
-}
+#define sample_number 1000
 
-Frame::~Frame()
-{
-    free(values);
-    delete (width);
-    delete (height);
-}
+using namespace std;
 
-Neuron::Neuron(int nrOfInputs)
+float sigmoid(double x)
 {
-    m_weights = (float *)malloc(nrOfInputs * sizeof(float));
-}
-Neuron::~Neuron()
-{
-    free(m_weights);
+    //cout<<x<<": ";
+    //cout<<1.0/(1+exp(-x))<<"; "<<endl;
+    return((float)1.0/((float)1.0+(float)exp(-x)));
 }
 
-Neuron::setNrOfNeurons(int value)
+void cout_char(char a)
 {
-    m_nrOfNeurons = value;
-}
-Neuron::setWeights(float *values)
-{
-    m_weights = values;
-}
-Neuron::setBias(float value)
-{
-    m_bias = value;
-}
-Neuron::updateWeight(int nrOfWeight, float value)
-{
-    m_weights[nrOfWeight] = value;
+    for (int i=7;i>=0;i--)
+        cout<<(1  & (a >> i))<<"";
+    cout<<endl;
 }
 
-Neuron::calcActivation(Frame input)
+class Image
 {
-    if (input == NULL)
+private:
+    float *tomb;
+    int image_value;
+public:
+    Image()
     {
-        cout << "No input provided!\n";
-        return 0;
+        tomb = (float*)malloc(sizeof(float)*784);
     }
-    else
+    ~Image()
     {
-        if (input.height * inpt.width != m_nrOfNeurons)
+        free(tomb);
+    }
+    void load(char * buffer, int start_p, int end_p)
+    {
+        for (int i=0;i<end_p-start_p;i++)
         {
-            cout << "Wrong input format!\n";
-            return 0;
+            tomb[i] = (float)buffer[i+start_p];
         }
-        else
+    }
+    float * get_values()
+    {
+        return tomb;
+    }
+    void set_image_value(int temp)
+    {
+        image_value = temp;
+    }
+    int get_image_value()
+    {
+        return image_value;
+    }
+    void print()
+    {
+        for (int i=0;i<784;i++)
         {
-            float sum = 0;
-            for (int i=0;i<input.width;i++)
+            if (tomb[i] == 0)
+                cout<<" ";
+            else
+                cout<<"#";
+            if (i%28==0 && i!=0)
+                cout<<endl;
+        }
+    }
+};
+
+class Layer
+{
+private:
+    float **net;
+    float *net_value;
+    float *weight_bias;
+    int weights,nodes;
+
+public:
+
+    static int aux;
+    int current;
+
+    Layer(int nodes_tmp, int weights_tmp)
+    {
+        weights = weights_tmp;
+        nodes = nodes_tmp;
+        net = (float **)malloc(sizeof(float*)*nodes_tmp);
+        for (int i=0;i<nodes_tmp;i++)
+        {
+            net[i] = (float*)malloc(sizeof(float)*weights_tmp);
+        }
+        net_value = (float *) malloc(sizeof(float)*nodes_tmp);
+        weight_bias = (float *) malloc(sizeof(float)*weights_tmp);
+        current = aux++;
+        cout<<"Layer "<<current<<" has been created with "<<nodes<<" nodes and "<<weights<<" weights, each."<<endl;
+
+    }
+    ~Layer()
+    {
+        for (int i=0;i<weights;i++)
+        {
+            free(net[i]);
+        }
+        free(net);
+        free(net_value);
+        free(weight_bias);
+    }
+    void random_fillup()
+    {
+        for (int i=0;i<nodes;i++)
+        {
+            for (int j=0;j<weights;j++)
             {
-                for (int j=0;j<input.height;j++)
-                {
-                    sum += input.values[i*width+j]*m_weights[i*width+j];
-                }
+                net[i][j] = (float)(rand()%200 -100)/100.0;
+                if (i==0)
+                    weight_bias[j] = (float)(rand()%200 -100)/100.0;
             }
-            sum += m_bias;
+            net_value[i] = 0.0;
+        }
+        cout<<"Layer "<<current<<" weight values had been updated randomly."<<endl;
+
+    }
+    void print()
+    {
+        for (int i=0;i<nodes;i++)
+        {
+            for (int j=0;j<weights;j++)
+            {
+                cout<<net[i][j]<<" ";
+            }
+            cout<<endl;
         }
     }
-    return sum;
-    //add sigmoid function and it's definition
+    void load_node_value(float *tmp)
+    {
+        net_value = tmp;
+        cout<<"Layer "<<current<<" node values have been updated."<<endl;
+    }
+    float * get_next_values()
+    {
+        float * tmp = (float*) malloc(sizeof(float)*weights);
+        double dtmp;
+        for (int i=0;i<weights;i++)
+        {
+            dtmp=0.0;
+            for (int j=0;j<nodes;j++)
+            {
+                dtmp += net_value[j]*net[j][i];
+            }
+            dtmp+=weight_bias[i];
+
+            tmp[i] = (float)sigmoid(dtmp);
+        }
+        return tmp;
+    }
+    float * get_node_values()
+    {
+        return net_value;
+    }
+    float get_node_weight(int n, int w)
+    {
+        return net[n][w];
+    }
+    void set_node_weight(int n, int w, float temp)
+    {
+        net[n][w] = temp;
+    }
+    float get_weight_bias(int n)
+    {
+        return weight_bias[n];
+    }
+    void set_weight_bias(int n, float b)
+    {
+        weight_bias[n] = b;
+    }
+};
+
+int Layer::aux = 1;
+
+void print_buffer(char *buffer, char * buffer_labels, int lSize)
+{
+    int i=0;
+    char ch1{176};
+    for (i=16;i<lSize;i++)
+    {
+        if (buffer[i]==0)
+        cout<<" ";
+        else
+        cout<<ch1;
+
+        if ((i-16)%28==0)
+        cout<<endl;
+        if ((i-16)%784 == 0)
+        {
+            cout<<"#########################################"<<endl;
+            cout<<buffer_labels[(i-16)/784+8]+0<<endl;
+            cout<<"#########################################"<<endl;
+        }
+    }
+}
+void print_images(int **images)
+{
+    for (int i=0;images[i] != NULL;i++)
+    {
+        for (int j=0;images[i][j]!=NULL;j++)
+        {
+            cout<<images[i][j];
+        }
+        cout<<endl;
+    }
+}
+int main ()
+{
+    FILE * pFile;
+    FILE * pFile_labels;
+
+    long lSize;
+    long lSize_labels;
+    char * buffer;
+
+    size_t result;
+    size_t result_labels;
+    char * buffer_labels;
+
+    pFile = fopen ( "images.txt" , "rb" );
+    pFile_labels = fopen ( "labels.txt" , "rb" );
+    if (pFile==NULL || pFile_labels==NULL) {fputs ("File error",stderr); exit (1);}
+
+    // obtain file size:
+    fseek (pFile , 0 , SEEK_END);
+    fseek (pFile_labels , 0 , SEEK_END);
+    lSize = ftell (pFile);
+    lSize_labels = ftell (pFile_labels);
+    rewind (pFile);
+    rewind (pFile_labels);
+
+    // allocate memory to contain the whole file:
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    buffer_labels = (char*) malloc (sizeof(char)*lSize_labels);
+    if (buffer == NULL || buffer_labels == NULL) {fputs ("Memory error",stderr); exit (2);}
+
+    // copy the file into the buffer:
+    result = fread (buffer,1,lSize,pFile);
+    result_labels = fread (buffer_labels,1,lSize_labels,pFile_labels);
+    if (result != lSize || result_labels != lSize_labels) {fputs ("Reading error",stderr); exit (3);}
+    cout<<"Image values loaded into \'buffer\' and labels into \'buffer_labels\'."<<endl;
+    /* the whole file is now loaded in the memory buffer. */
+
+    //print_buffer(buffer, buffer_labels, lSize);
+
+    //loading values
+    Image X[sample_number];
+    for (int i=0;i<sample_number;i++)
+    {
+        X[i].load(buffer,16+(i*784),16+((i+1)*784));
+        X[i].set_image_value(buffer_labels[i+8]);
+    }
+    free(buffer);
+    cout<<"Images segmented."<<endl;
+
+
+
+    Layer layer_1(784,30);
+    layer_1.random_fillup();
+    Layer layer_2(30,10);
+    layer_2.random_fillup();
+
+    layer_1.load_node_value(X[1].get_values());
+
+    float * value_temp = layer_1.get_next_values();
+    cout<<"Layer 2: ";
+    for (int i=0;i<30;i++)
+    {
+        cout<<value_temp[i]<<"  ";
+    }
+    cout<<endl;
+    layer_2.load_node_value(value_temp);
+
+    value_temp = layer_2.get_next_values();
+    cout<<"Layer 3: ";
+    for (int i=0;i<10;i++)
+    {
+        cout<<value_temp[i]<<"  ";
+    }
+
+
+
+    // terminate
+    fclose (pFile);
+    free (buffer);
+    return 0;
 }
