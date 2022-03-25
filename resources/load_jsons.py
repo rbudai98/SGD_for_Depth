2a20bb915666c903b04ac38959b398cd11bb34a4
rbudai98 <robert.budai@analog.com>
2022-03-25 14:58:04 +0200

    dataSet format corrected, training on multipple dataSets
    Signed-off-by: rbudai98 <robert.budai@analog.com>

--- resources/load_jsons.py
+++ resources/load_jsons.py
@@ -18,6 +18,7 @@ import array
 import numpy as np
 import tensorflow as tf
 import re
+import matplotlib.pyplot as plt
 
 dataSetArraysOk = np.array([])
 
@@ -27,146 +28,162 @@ nrOfCroppedFileOk = 0
 nrOfCroppedFileNotOk = 0
 outputArray = np.array([])
 
+for i in range (2,5):
+    nr_of_dataset = i
+    print("DataSet: ", nr_of_dataset)
+    dataset_path = "dataSet" + (str)(nr_of_dataset) + "/"
+    ir_path = "ir/ir_"
+    depth_path = "depth/depth_"
+    json_path = "jsons/ir_"
 
-nr_of_dataset = 4
-dataset_path = "dataSet" + (str)(nr_of_dataset) + "/"
-ir_path = "ir/ir_"
-depth_path = "depth/depth_"
-json_path = "jsons/ir_"
+    for filename in os.listdir(os.getcwd() + "/" + dataset_path + "jsons"):
+        with open(os.path.join(os.getcwd()+"/" + dataset_path + "jsons", filename), 'r') as f:  # open in readonly mode
 
-for filename in os.listdir(os.getcwd() + "/" + dataset_path + "jsons"):
-    with open(os.path.join(os.getcwd()+"/" + dataset_path + "jsons", filename), 'r') as f:  # open in readonly mode
+            file_number = os.path.splitext(filename)[0]
 
-        file_number = os.path.splitext(filename)[0]
+            emp_str = ""
+            for m in file_number:
+                if m.isdigit():
+                    emp_str = emp_str + m
+            k = (int)(emp_str)
 
-        emp_str = ""
-        for m in file_number:
-            if m.isdigit():
-                emp_str = emp_str + m
-        k = (int)(emp_str)
+            f = open(dataset_path + json_path + (str)(k) + ".json")
 
-        f = open(dataset_path + json_path + (str)(k) + ".json")
+            # returns JSON object as
+            # a dictionary
+            data = json.load(f)
 
-        # returns JSON object as
-        # a dictionary
-        data = json.load(f)
+            # Iterating through the json
+            # list
+            # for i in data['emp_details']:
+            # print(data["shapes"])
+            x1 = round(data["shapes"][0]['points'][0][0])
+            y1 = round(data["shapes"][0]['points'][0][1])
+            x2 = round(data["shapes"][0]['points'][1][0])
+            y2 = round(data["shapes"][0]['points'][1][1])
 
-        # Iterating through the json
-        # list
-        # for i in data['emp_details']:
-        # print(data["shapes"])
-        x1 = round(data["shapes"][0]['points'][0][0])
-        y1 = round(data["shapes"][0]['points'][0][1])
-        x2 = round(data["shapes"][0]['points'][1][0])
-        y2 = round(data["shapes"][0]['points'][1][1])
+            if (y2 < y1):
+                y3 = y2
+                y2 = y1
+                y1 = y3
+            if (x2 < x1):
+                x3 = x2
+                x2 = x1
+                x1 = x3
 
-        if (y2 < y1):
-            y3 = y2
-            y2 = y1
-            y1 = y3
-        if (x2 < x1):
-            x3 = x2
-            x2 = x1
-            x1 = x3
+            if (abs(x2-x1) > 320 or abs(y2-y1)>240):
+                size = (640, 480)
+            elif (abs(x2-x1) > 160 or abs(y2-y1)>120):
+                size = (320, 240)
+            elif (abs(x2-x1) > 80 or abs(y2-y1)>60):
+                size = (160, 120)
+            elif (abs(x2-x1) > 40 or abs(y2-y1)>30):
+                size = (80, 60)
+            elif (abs(x2-x1) > 20 or abs(y2-y1)>15):
+                size = (40, 30)
+            else:
+                size = (20, 15)
+            print(k, ". ", x2-x1, y2-y1, "size=", size)
 
-        if (abs(x2-x1) > 320):
-            size = (640, 480)
-        elif abs(x2-x1) > 160:
-            size = (320, 240)
-        elif abs(x2-x1) > 80:
-            size = (160, 120)
-        elif abs(x2-x1) > 40:
-            size = (80, 60)
-        elif abs(x2-x1) > 20:
-            size = (40, 30)
-        else:
-            size = (20, 15)
-        print(k, ". ", x2-x1, y2-y1, "size=", size)
+            # IR frame
+            irFileName = dataset_path + ir_path + (str)(k) + ".png"
+            irFrame = cv2.imread(irFileName)
 
-        # IR frame
-        irFileName = dataset_path + ir_path + (str)(k) + ".png"
-        irFrame = cv2.imread(irFileName)
+            # depth frame
+            depthFileName = dataset_path + depth_path + (str)(k) + ".bin"
+            f = open(depthFileName, "r")
 
-        # depth frame
-        depthFileName = dataset_path + depth_path + (str)(k) + ".bin"
-        f = open(depthFileName, "r")
+            data = f.buffer.read(640*480*2)
+            # reading in into cv2:Mat object the frame
+            depthFrame = np.frombuffer(
+                #data, dtype=np.uint16).reshape((480, 640, 1))
+                data, dtype=np.uint16).reshape((480, 640))
+            # converting the frame into 8 bit array (255/4055) = alpha, hence deft = src*alpha
+            
+            #depthFrame = cv2.convertScaleAbs(depthFrame, alpha=0.063)
+            depthFrame = (depthFrame/4).astype('uint8')
+            #depthFrame = depthFrame.astype(np.uint8)
+            
+            
+            '''heatmapshow = None
+            print("Max of matrices: ", np.max(depthFrame), ", min of matrices: ", np.min(depthFrame))
+            heatmapshow = cv2.normalize(depthFrame, heatmapshow, alpha=np.min(depthFrame), beta=np.max(depthFrame), norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            heatmapshow = cv2.applyColorMap(heatmapshow, cv2.COLORMAP_JET)
+            cv2.imshow("Depth map", heatmapshow)
+            cv2.imshow("IR map", irFrame)
+            cv2.waitKey(0)'''
 
-        data = f.buffer.read(640*480*2)
-        # reading in into cv2:Mat object the frame
-        depthFrame = np.frombuffer(
-            data, dtype=np.uint16).reshape((480, 640, 1))
-        # converting the frame into 8 bit array (255/4055) = alpha, hence deft = src*alpha
+    
 
-        #depthFrame = cv2.convertScaleAbs(depthFrame, alpha=0.063)
-        depthFrame = depthFrame * 255/1023
-        depthFrame = depthFrame.astype(np.uint8)
+            for i in range((int)((640-size[0])/(size[0]/2)+1)):
+                for j in range((int)((480-size[1])/(size[1]/2)+1)):
+                    x_cord = (int)((i)*(size[0]/2))
+                    y_cord = (int)((j)*(size[1]/2))
 
-        for i in range((int)((640-size[0])/(size[0]/2)+1)):
-            for j in range((int)((480-size[1])/(size[1]/2)+1)):
-                x_cord = (int)((i)*(size[0]/2))
-                y_cord = (int)((j)*(size[1]/2))
+                    # cropping the regions of the image
+                    irCropOriginal = irFrame[y_cord:(
+                        (y_cord+size[1])), x_cord:(x_cord+size[0])]
+                    depthCropOriginal = depthFrame[y_cord:(
+                        (y_cord+size[1])), x_cord:(x_cord+size[0])]
+                    # resizing to have the same size
+                    irCrop = cv2.resize(
+                        irCropOriginal, (20, 15), interpolation=cv2.INTER_LINEAR)
+                    depthCrop = cv2.resize(
+                        depthCropOriginal, (20, 15), interpolation=cv2.INTER_LINEAR)
 
-                # cropping the regions of the image
-                irCrop = irFrame[y_cord:(
-                    (y_cord+size[1])), x_cord:(x_cord+size[0])]
-                depthCrop = depthFrame[y_cord:(
-                    (y_cord+size[1])), x_cord:(x_cord+size[0])]
-                # resizing to have the same size
-                irCrop = cv2.resize(
-                    irCrop, (20, 15), interpolation=cv2.INTER_LINEAR)
-                depthCrop = cv2.resize(
-                    depthCrop, (20, 15), interpolation=cv2.INTER_LINEAR)
+                    # calculate the next region to check if it includes the right boundbox
+                    x_cord_next = (int)((i+1)*(size[0]/2))
+                    y_cord_next = (int)((j+1)*(size[1]/2))
 
-                # calculate the next region to check if it includes the right boundbox
-                x_cord_next = (int)((i+1)*(size[0]/2))
-                y_cord_next = (int)((j+1)*(size[1]/2))
+                    # if contains we have as output 1
+                    if x1 >= x_cord and x1 < x_cord_next and y1 >= y_cord and y1 < y_cord_next:
+                        # found. right bounding box
+                        # compensate found boxes, copy object multipple times
+                        for l in range((int)((640-size[0])/(size[0]/2)+1)*(int)((480-size[1])/(size[1]/2)+1) - 1):
+                            #irCroppedFileName = "../ir_cropped/test_ir_"+(str)(nrOfCroppedFileOk)+".jpg"
+                            # with open("../depth_cropped/1/1_image_"+(str)(nrOfCroppedFileOk)+".jpg", "wb") as file:
+                            #    file.write(bytearray(depthCrop))
+                            # file.close()
+                            
+                            #cv2.imshow("ir", irCropOriginal)
+                            #cv2.imshow("depth", depthCropOriginal) 
+                            #cv2.waitKey(0)
 
-                # if contains we have as output 1
-                if x1 >= x_cord and x1 < x_cord_next and y1 >= y_cord and y1 < y_cord_next:
-                    # found. right bounding box
-                    # compensate found boxes, copy object multipple times
-                    for l in range((int)((640-size[0])/(size[0]/2)+1)*(int)((480-size[1])/(size[1]/2)+1) - 1):
-                        #irCroppedFileName = "../ir_cropped/test_ir_"+(str)(nrOfCroppedFileOk)+".jpg"
-                        # with open("../depth_cropped/1/1_image_"+(str)(nrOfCroppedFileOk)+".jpg", "wb") as file:
+                            outputArray = np.append(outputArray, 1)
+
+                            depthCrop = np.reshape(np.asarray(
+                                depthCrop), (1, 300)).flatten()
+                            dataSetArraysOk = np.append(dataSetArraysOk, depthCrop)
+
+                            nrOfCroppedFileOk = nrOfCroppedFileOk + 1
+
+                    # in other case 0
+                    else:
+                        # not found, not rigth bounding box
+                        #irCroppedFileName = "../ir_cropped/test_ir_"+(str)(nrOfCroppedFileNotOk)+".jpg"
+                        # with open("../depth_cropped/0/0_image_"+(str)(nrOfCroppedFileNotOk)+".jpg", "wb") as file:
                         #    file.write(bytearray(depthCrop))
                         # file.close()
                         #cv2.imwrite(irCroppedFileName, irCrop)
-                        outputArray = np.append(outputArray, 1)
 
+                        outputArray = np.append(outputArray, 0)
                         depthCrop = np.reshape(np.asarray(
                             depthCrop), (1, 300)).flatten()
                         dataSetArraysOk = np.append(dataSetArraysOk, depthCrop)
 
-                        nrOfCroppedFileOk = nrOfCroppedFileOk + 1
-
-                # in other case 0
-                else:
-                    # not found, not rigth bounding box
-                    #irCroppedFileName = "../ir_cropped/test_ir_"+(str)(nrOfCroppedFileNotOk)+".jpg"
-                    # with open("../depth_cropped/0/0_image_"+(str)(nrOfCroppedFileNotOk)+".jpg", "wb") as file:
-                    #    file.write(bytearray(depthCrop))
-                    # file.close()
-                    #cv2.imwrite(irCroppedFileName, irCrop)
-
-                    outputArray = np.append(outputArray, 0)
-                    depthCrop = np.reshape(np.asarray(
-                        depthCrop), (1, 300)).flatten()
-                    dataSetArraysOk = np.append(dataSetArraysOk, depthCrop)
-
-                    nrOfCroppedFileNotOk = nrOfCroppedFileNotOk + 1
+                        nrOfCroppedFileNotOk = nrOfCroppedFileNotOk + 1
 
 
-np.save(dataset_path + "dataSetArray", dataSetArraysOk)
-np.save(dataset_path + "dataSetArraysOutput", outputArray)
+    np.save(dataset_path + "dataSetArray", dataSetArraysOk)
+    np.save(dataset_path + "dataSetArraysOutput", outputArray)
+
+    myDataSet = tf.data.Dataset.from_tensor_slices(
+        np.reshape(dataSetArraysOk, (-1, 300)))
+    myDataSetLabel = tf.data.Dataset.from_tensor_slices(outputArray)
+
+    print("Dataset cardinality: ", myDataSet.cardinality().numpy())
+    print("Dataset label cardinality: ", myDataSetLabel.cardinality().numpy())
 
 
-myDataSet = tf.data.Dataset.from_tensor_slices(
-    np.reshape(dataSetArraysOk, (-1, 300)))
-myDataSetLabel = tf.data.Dataset.from_tensor_slices(outputArray)
-
-print("Dataset cardinality: ", myDataSet.cardinality().numpy())
-print("Dataset label cardinality: ", myDataSetLabel.cardinality().numpy())
-
-
-# print(outputArray)
-savetxt(dataset_path + "outputArray.csv", outputArray.astype(np.uint8), delimiter=" ")
+    # print(outputArray)
+    savetxt(dataset_path + "outputArray.csv", outputArray.astype(np.uint8), delimiter=" ")
--- tensorflow/CNN_trainin.py
+++ tensorflow/CNN_trainin.py
@@ -6,10 +6,17 @@ from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import os
+from sklearn import utils
 
 
 img_height = 20
 img_width = 15
+
+
+def shuffle(a,b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 ### MODEL
 
@@ -21,7 +28,7 @@ outputs = layers.Dense(1)(x2)
 model = keras.Model(inputs=inputs, outputs=outputs, name="fully_connected")
 model.summary()
 # Instantiate an optimizer.
-optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+optimizer = keras.optimizers.SGD(learning_rate=2e-3)
 # Instantiate a loss function.
 loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 # Restore the weights
@@ -40,31 +47,40 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 
 ### DATA SET
 
-dataset_number = 4
-dataset_path = "../resources/dataSet" + (str)(dataset_number) + "/"
-# reading in from file
-dataSetX = np.load(dataset_path + "dataSetArray.npy")
-dataSetY = np.load(dataset_path + "dataSetArraysOutput.npy")
-nr_of_imgs = (int)(dataSetX.size/300)
-dataSetX = dataSetX.reshape(nr_of_imgs, 300).astype("float32") / 4095
-print("################\nNr of lengths: ", nr_of_imgs)
+for i in range(2,5):
 
-### TRAINING
-history = model.fit(dataSetX, 
-                    dataSetY,
-                    batch_size=1,
-                    epochs=10,
-                    validation_split=0.2,   
-                    verbose=1,
-                    callbacks=[cp_callback],
-                    shuffle=True)
-test_scores = model.evaluate(dataSetX, dataSetY, verbose=1)
+    print("\n###\n\n DataSet: ", i, "\n\n")
+    dataset_number = i
+    dataset_path = "../resources/dataSet" + (str)(dataset_number) + "/"
 
-# Save the weights
-model.save_weights('./checkpoints/my_checkpoint')
-model.save('saved_model/my_model')
+    # reading in from file
+    dataSetX = np.load(dataset_path + "dataSetArray.npy")
+    dataSetY = np.load(dataset_path + "dataSetArraysOutput.npy")
 
-print("Test loss:", test_scores[0])
-print("Test accuracy:", test_scores[1])
+    print("Size: ",dataSetX.size)
+    nr_of_imgs = (int)(dataSetX.size/300)
+    dataSetX = dataSetX.reshape(nr_of_imgs, 300)
+
+
+    print("################\nNr of images: ", nr_of_imgs)
+
+
+    ### TRAINING
+    history = model.fit(dataSetX, 
+                        dataSetY,
+                        batch_size=5,
+                        epochs=100,
+                        validation_split=0.2,   
+                        verbose=2,
+                        callbacks=[cp_callback],
+                        shuffle=True)
+    test_scores = model.evaluate(dataSetX, dataSetY, verbose=1)
+
+    # Save the weights
+    model.save_weights('./checkpoints/my_checkpoint')
+    model.save('saved_model/my_model')
+
+    print("Test loss:", test_scores[0])
+    print("Test accuracy:", test_scores[1])
 
 
