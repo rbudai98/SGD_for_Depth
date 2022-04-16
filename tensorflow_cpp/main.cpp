959aa2fec2970ad69abe7c67a82b0d47415cbd3b
rbudai98 <robert.budai@analog.com>
2022-04-17 02:43:07 +0300

    Added cppflow
    Signed-off-by: rbudai98 <robert.budai@analog.com>

---
+++ tensorflow_cpp/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "cppflow/cppflow.h"
+
+
+int main() {
+
+    // Load the model
+    cppflow::model model("../tensorflow/saved_model/my_model-20220405-111254");
+
+    float test[300] = {1.f};
+    std::vector<float> test2(std::begin(test), std::end(test));
+    std::vector<int64_t> size;
+    size.push_back(300);
+    auto input = cppflow::tensor(test2, size);
+    // Load an image
+    //auto input = cppflow::decode_jpeg(cppflow::read_file(std::string("image.jpg")));
+
+
+    // Cast it to float, normalize to range [0, 1], and add batch_dimension
+    auto input2 = cppflow::cast(input, TF_UINT8, TF_FLOAT);
+    //input = input / 255.f;
+    input2 = cppflow::expand_dims(input2, 0);
+
+    // Run
+    auto output = model(input2);
+    
+    // Show the predicted class
+    std::cout << cppflow::arg_max(output, 1) << std::endl;
+}
