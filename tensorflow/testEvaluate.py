5e4de568ed9258055d3266f43b8f5cc7dff89784
rbudai98 <robert.budai@analog.com>
2022-04-17 22:49:10 +0300

    added jsonify
    Signed-off-by: rbudai98 <robert.budai@analog.com>

--- tensorflow/testEvaluate.py
+++ tensorflow/testEvaluate.py
@@ -34,50 +34,25 @@ def randomize(x, y):
 
 inputs = keras.Input(shape=(300,))
 inputs.shape
-x = layers.Dense(300, activation="sigmoid")(inputs)
-x2 = layers.Dense(300, activation="sigmoid")(x)
+x = layers.Dense(128, activation="sigmoid")(inputs)
+x1 = layers.Dense(128, activation="sigmoid")(x)
+x2 = layers.Dense(128, activation="sigmoid")(x1)
 outputs = layers.Dense(1, activation="sigmoid")(x2)
 model = keras.Model(inputs=inputs, outputs=outputs, name="fully_connected")
+
+# Restore the weights
+model.load_weights('./checkpoints/my_checkpoint-20220417-150829')
+
 model.summary()
 
-# Instantiate a loss function.
-#loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-loss_fn = tf.losses.mae
-# Restore the weights
-model.load_weights('./checkpoints/my_checkpoint-20220404-185619')
-model.compile(
-    loss=keras.losses.BinaryCrossentropy(),
-    metrics=["accuracy"],
-)
+print(model.get_weights())
 
-# DATA SET
+#for layer in model.layers:
+    #print(layer.name, layer)
 
-dataset_number = 2
-dataset_path = "../resources/dataSet" + (str)(dataset_number) + "/"
-
-# reading in from file
-dataSetX = np.load(dataset_path + "dataSetArray.npy")
-dataSetY = np.load(dataset_path + "dataSetArraysOutput.npy")
-
-print("Size: ", dataSetX.size, ", max value= ", np.max(dataSetX), "\n\n")
-nr_of_imgs = (int)(dataSetX.size/300)
-dataSetX = (dataSetX.reshape(nr_of_imgs, 300) /
-            (np.max(dataSetX))).astype("float32")
-dataSetY = dataSetY.astype("float32")
-(dataSetX, dataSetY) = randomize(dataSetX, dataSetY)
-
-nr_of_test=10
-prediction = model.predict(dataSetX[:10])
-print("Prediction       Labels")
-
-for i in range(nr_of_test):
-    print(prediction[i], "      ", dataSetY[i])
-
-
-
-
-
-
-# Save the weights
-model.save_weights('./checkpoints/my_checkpoint-'+ name_specific)
-model.save('saved_model/my_model-'+ name_specific)
+for layer in model.layers[1:]:
+    #print(layer.weights)
+    print(keras.layers.serialize(layer))
+    print(layer.bias.numpy())
+    print("###################################################################")
+  
